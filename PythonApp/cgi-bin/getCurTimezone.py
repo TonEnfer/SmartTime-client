72bd6cf1dfc520886262ee331d0d445d78235e1a
Anton <tonik.sam@gmail.com>
2017-06-10 20:14:00 +0500

    Изменил скрипт получения текущей часовой зоны

--- PythonApp/cgi-bin/getCurTimezone.py
+++ PythonApp/cgi-bin/getCurTimezone.py
@@ -5,25 +5,9 @@ import os.path
 import sys 
 import json
 
-
-def main(argv):
-	tzname = os.environ.get('TZ')
-	print("Content-type: text/html")
-	res = "none"
-	if tzname:
-		res = json.dumps({'TZ':tzname})
-		print(res)
-	elif os.path.exists('/etc/timezone'):
-		f = open('/etc/timezone',"r")
-		tzname =  f.read()
-		res = json.dumps({'TZ':tzname})
-		print(res)
-	else:
-		print("res)
-		sys.exit(1)
-	#res = "1"#json.dumps(({'TZ':"none"})
-	
-
-
-if __name__ == '__main__':
-	main(sys.argv)
+print("Content-type: text/html\n")
+f = open('/etc/timezone',"r")
+tzname =  f.read().replace('\n','')
+Area,City = tzname.split('/')
+res = json.dumps({'Area':Area,'City':City})
+print(res)
