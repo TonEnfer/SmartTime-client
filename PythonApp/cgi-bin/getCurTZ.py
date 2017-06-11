e834ca355cdf21e8212d8a5453803f3193e88902
TonEnfer <tonik.sam@gmail.com>
2017-06-11 19:23:56 +0500

    Исправил ошибку в пути к БД

--- PythonApp/cgi-bin/getCurTZ.py
+++ PythonApp/cgi-bin/getCurTZ.py
@@ -2,7 +2,7 @@
 
 import json
 import sqlite3
-db = sqlite3.connect("../../DB/db.sqlite")
+db = sqlite3.connect("../DB/db.sqlite")
 cursor = db.cursor()
 print("Content-type: text/html\n")
 cursor.execute("SELECT Value FROM Parameters WHERE name = 'TZ'")
