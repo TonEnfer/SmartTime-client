8e9479b99be77ea02d59109e0beae93814084384
TonEnfer <tonik.sam@gmail.com>
2017-06-11 19:38:07 +0500

    Изменил скрипт для работы с DB

--- PythonApp/cgi-bin/setTZ.py
+++ PythonApp/cgi-bin/setTZ.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 
-import os
-import os.path
 import cgi
 import json
+import sqlite3
+
+db = sqlite3.connect("../DB/db.sqlite")
+cursor = db.cursor()
 
 form = cgi.FieldStorage()
 TZ = form.getfirst("TZ", '')
-res = "ERROR"
-if(TZ != ''):
-	f = open('/etc/timezone',"w")
-	f = TZ
-	res = "OK"
 
-res = json.dumps({"RESULT":res})
+res = "error"
+if(TZ != ''):
+	query = "UPDATE Parameters SET value = '{}' WHERE name = 'TZ'".format(TZ)
+	cursor.execute(query);
+	#res = TZ
+db.commit()
+db.close()
+res = json.dumps({"RESULT":TZ})
 print("Content-type: text/html\n")
 print(res)
