9b65359ddf25674046bfba34b25b1a0d60783813
TonEnfer <tonik.sam@gmail.com>
2017-06-11 19:17:37 +0500

    Изменил скрипт получения списка часовых поясов

--- PythonApp/cgi-bin/getTZList.py
+++ PythonApp/cgi-bin/getTZList.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
-import json
-import subprocess
 
-#args = .split()
+import json
+import sqlite3
+import os
 print("Content-type: text/html\n")
-pr = subprocess.Popen("timedatectl list-timezones", shell=True, stdout=subprocess.PIPE)
-res = pr.communicate()[0].decode("utf-8").replace('\n','\n\r')
-a = res.split()
-b = '{TZ : ['
-for i in a:
-	 b += ("\"" + i + "\", ")
-b = b[:-2] + "] }"
-print(b)
+#print(os.getcwd());
+db = sqlite3.connect("../DB/db.sqlite")
+cursor = db.cursor()
+
+tznames = [str(i)[2:-3] for i in cursor.execute("SELECT name FROM TimeZone")]
+# = cursor.fetchone()
+#print(tzname)
+res = '{TZ : ['
+for tz in tznames:
+	res += ("\"" + tz + "\", ")
+res = res[:-2] + "] }"
+#res = json.dumps({'TZ':tzname})
+
+print(res)
+
+#db.close()
